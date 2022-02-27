0f2fdcb9862e8e44d68459b6287148879b46baa4
lazynorie <chengjy1991@gmail.com>
2022-02-26 23:37:36 -0500

    implemented start menu functions

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainmenuPanel;
+    [SerializeField]
+    private GameObject instructionPanel;
+    [SerializeField]
+    private GameObject creditPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainmenuPanel.SetActive(true);
+        instructionPanel.SetActive(false);
+        creditPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OnCreditButtonClicked()
+    {
+        mainmenuPanel.SetActive(false);
+        creditPanel.SetActive(true);
+    }
+
+    public void OnBackButtonClicked()
+    {
+        mainmenuPanel.SetActive(true);
+        creditPanel.SetActive(false);
+        instructionPanel.SetActive(false);
+    }
+
+    public void OnInstructionButtonClicked()
+    {
+        mainmenuPanel.SetActive(false);
+        creditPanel.SetActive(false);
+        instructionPanel.SetActive(true);
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }
