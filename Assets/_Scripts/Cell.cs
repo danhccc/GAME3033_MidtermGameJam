18a0f56c55a80f848f55b172b1a67bd0d2df3b7e
danhc <daniel94126@hotmail.com>
2022-02-27 18:00:39 -0500

    change cell behaviours

--- Assets/_Scripts/Cell.cs
+++ Assets/_Scripts/Cell.cs
@@ -7,16 +7,21 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     [SerializeField]
-    private bool isColliding = false;
+    private bool isBlocked = false;
 
     [SerializeField]
     private bool isMoving = false;
 
     [SerializeField] private float moveSpeed = 1.0f;
     
-    [SerializeField] private float waitTimeBeforeFall = 1.0f;
+    [SerializeField] private float waitTimeBeforeFall = 0.5f;
+
+    private Vector3 originPosition;
+    private Vector3 originScale;
 
     public PLATFORMTYPE platformType;
+
+    private Rigidbody rigid;
 
     public Material ZMaterial;
     public Material XMaterial;
@@ -26,6 +31,9 @@ public class Cell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        originPosition = transform.position;
+        originScale = transform.localScale;
+        rigid = GetComponent<Rigidbody>();
         render = GetComponent<Renderer>();
         SetMaterial();
     }
@@ -44,32 +52,45 @@ public class Cell : MonoBehaviour
     IEnumerator moveNow()
     {
         yield return new WaitForSeconds(waitTimeBeforeFall);
-        
-        Vector3 originalPos = transform.position;
-        if (isMoving == false)
-        {
-            isMoving = true;
-        }
+        isMoving = true;
+
+//         if (isMoving == false)
+//         {
+//             isMoving = true;
+//         }
     }
 
     
 
     private void OnCollisionEnter(Collision other)
     {
+        if (other.gameObject.CompareTag("Cell"))
+        {
+            isBlocked = true;
+            return;
+        }
         if (other.gameObject.tag == "Player")
         {
-            other.transform.parent = this.transform;
+            //other.transform.parent = this.transform;
             Move();
         }
+
     }
+    
 
     private void OnCollisionExit(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.transform.parent = null;
-            Debug.Log("Exit");
+            //Debug.Log("Exit");
             StartCoroutine("StopNow");
+        }
+
+        if (other.gameObject.CompareTag("Cell"))
+        {
+            isBlocked = false;
+            return;
         }
     }
     IEnumerator StopNow()
@@ -77,10 +98,13 @@ public class Cell : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         isMoving = false;
     }
+
+
     private void MovePlatform()
     {
         if (isMoving)
         {
+            if (isBlocked) return;
             switch (platformType)
             {
                 case PLATFORMTYPE.YAXIS:
@@ -91,6 +115,7 @@ public class Cell : MonoBehaviour
                     break;
                 case PLATFORMTYPE.ZAXIS:
                     transform.position = transform.position + new Vector3(0,0,Time.deltaTime * moveSpeed);
+                    //transform.localScale = new Vector3(Time.deltaTime /10, originScale.y, Time.deltaTime/10);
                     break;
                 default:
                     break;
